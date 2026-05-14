9bf473e46e7f98a9c21186ee898d2b408c407fce
Cristopher Rondon Añaños <89040296+Cris200399@users.noreply.github.com>
2026-05-14 18:18:08 -0500

    feat: GitHub Actions deploy a GitHub Pages

--- lib/config/app_config.dart
+++ lib/config/app_config.dart
@@ -1,10 +1,8 @@
 class AppConfig {
-  // static const String baseUrl     = 'http://localhost:3000';
-  static const String baseUrl     = 'https://vascular-habitat-correct.ngrok-free.dev';
-  static const String apiUrl      = '$baseUrl/api';
-  static const String socketUrl   = baseUrl;
+  static const String baseUrl   = 'https://riego-backend-production.up.railway.app';
+  static const String apiUrl    = '$baseUrl/api';
+  static const String socketUrl = baseUrl;
 
-  // Timeouts
   static const int connectTimeout = 10000;
   static const int receiveTimeout = 10000;
 }
